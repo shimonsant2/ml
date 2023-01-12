9bd29837722d60a2e0257ff79f73d0aabce27e3b
Shimon Sant <122385952+shimonsant2@users.noreply.github.com>
2023-01-12 17:31:38 +0200

    Modify the iris.csv path to the correct ml project

--- ml.py
+++ ml.py
@@ -8,7 +8,7 @@ from sklearn.svm import SVC
 
 # Load dataset
 # url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
-url = "/Users/shimonsant/PycharmProjects/pythonProject/iris.csv"
+url = "/Users/shimonsant/PycharmProjects/ml/iris.csv"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = read_csv(url, names=names)
 print(dataset)
